94e7456d3dc46b1437b53039d06280f5e6d0df48
guoxinling <guoxinling_xisu@163.com>
2026-03-25 23:23:01 +0800

    Update docusaurus.config.ts

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -8,7 +8,7 @@ const config: Config = {
   favicon: 'img/favicon.ico',
 
   url: 'https://guoxinling.github.io',
-  baseUrl: '/',
+  baseUrl: '/aiwiki/',
 
   organizationName: 'guoxinling',
   projectName: 'aiwiki',
